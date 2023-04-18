716d9d6fb9ed3633298cfa08a062957d998a977c
MariaS <107494271+MariaSchroder@users.noreply.github.com>
2023-04-18 12:13:50 +0200

    Guitarist_Portfolio

---
+++ js/script.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav-menu'),
+    menuItem = document.querySelectorAll('.nav-menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav-menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+})
+
+
+
+// smooth scrolling
+
+  $(document).ready(function(){
+  $("a[href*='#']").on("click", function(e){
+    var anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 1777);
+    e.preventDefault();
+    return false;
+  });
+});
+
+
